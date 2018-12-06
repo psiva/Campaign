38a0ad4b4251fa469a2ced641235000639f6cd54
Siva Puvvada <siva.puvvada@gmail.com>
2018-12-05 21:22:27 -0500

    Updated Campaign Contract, README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Campaign
+# CrowdCoin
 Kickstarter Clone using Ethereum
+
+CrowdCoin allows crowdsourcing funds for use in their projects. There are 2 different roles in this project
+
+Owner - Creator of a project/campaign
+Contributer/Approver - One who contributed to a campaign and has an approving authority
+
+When an owner needs money to spend, he creates a request for the release of funds. Contributors can
+approve requests and requests that are approved by more than half of contributors can then be finalized
+by the owner to release the funds.
--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,6 +1,16 @@
 pragma solidity ^0.4.3;
 
-contract Campaign{
+import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+/**
+@title Contract to implement functionality related to management of a campaign. 
+@dev   Extends Pausable contract to implement Emergency Stop Pattern
+@author Siva Puvvada
+ */
+contract Campaign is Pausable{
+
+    using SafeMath for uint;
     
     struct Request{
         string description;
@@ -17,6 +27,7 @@ contract Campaign{
     mapping(address=>bool) public approvers;
     Request[] public requests;
     
+    event RequestCreated(string description,uint value, address recipient);
     event RequestFinalized(address recipient,uint value);
 
     modifier onlyOwner{
@@ -29,12 +40,23 @@ contract Campaign{
         minimumContribution = minimumAmount;
     }
     
+    /**
+    @notice This function allows a user to contribute to a campaign if he is contributing a minimum amount
+           as defined by the owner of the campaign
+    @author Siva Puvvada
+     */
     function contribute() payable public {
         require(msg.value > minimumContribution);
         approvers[msg.sender] = true;
-        approversCount++;
+        approversCount = approversCount.add(1);
     }
     
+    /**
+    @notice Allows contract's owner to create a request for release of funds to a recipient
+    @param description Description of the request
+    @param value Amount required for this request
+    @param recipient Vendor who provides goods/services in return of funds
+     */
     function createRequest(string memory description, uint value, address recipient) 
         public onlyOwner 
     {
@@ -47,30 +69,44 @@ contract Campaign{
         });
         
         requests.push(request);
+        emit RequestCreated(request.description,request.value,request.recipient);
     }
     
+    /**
+    @notice Allows a contributor to the campaign approve a request created by the owner
+    @param index Index of the request in the request array that contributor wants to approve
+     */
     function approveRequest(uint index) public {
-        
+
         Request storage request = requests[index];
-        
         require(approvers[msg.sender]);
         require(!request.approvals[msg.sender]);
-        
         request.approvals[msg.sender]=true;
-        request.approvalCount++;
-        
+        request.approvalCount = (request.approvalCount).add(1);
     }
     
-    function finalizeRequest(uint index) public onlyOwner{
+    /**
+    @notice Allows the owner to finalize the request if the more than 50% contributors approve the 
+            request and release the funds to the recipient defined in the request
+    @dev    using whenNotPaused modifier from Pausable contract for implementing emergency stop pattern
+            performing contract related work before calling transfer to prevent reentrancy attacks
+    @param index Index of the request in the request array that needs to be finalized
+     */
+    function finalizeRequest(uint index) 
+        public onlyOwner whenNotPaused{
         Request storage request = requests[index];
         require(request.approvalCount>(approversCount/2));
         require(!request.isComplete);
         request.isComplete = true;
-        emit RequestFinalized(request.recipient,request.value);
         address receiver = request.recipient;
         receiver.transfer(request.value);
+        emit RequestFinalized(request.recipient,request.value);
     }
 
+    /**
+    @notice Publishes the summary of the campaign
+    @return Minimum Contribution, Balance, Number of requests, Number of approvers and Owner's address
+     */
     function getSummary() public view returns(uint,uint,uint,uint,address) {
         return(
             minimumContribution,
@@ -81,6 +117,12 @@ contract Campaign{
         );
     }
 
+    /**
+    @notice Returns the number of requests
+    @dev As Solidity does not return the entire array, getting the count to make individual calls 
+         for retrieving the details of the requests
+    @return Number of requests created for a particular campaign
+     */
     function getRequestCount() public view returns(uint){
         return requests.length;
     }
